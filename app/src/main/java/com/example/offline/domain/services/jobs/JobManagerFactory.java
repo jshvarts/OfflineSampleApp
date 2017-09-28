44ad230b0d83936d74d9746954be5c0724971655
James Shvarts <jshvarts@gmail.com>
2017-09-28 13:53:09 -0400

    extract customLogger into a member var

--- app/src/main/java/com/example/offline/domain/services/jobs/JobManagerFactory.java
+++ app/src/main/java/com/example/offline/domain/services/jobs/JobManagerFactory.java
@@ -24,39 +24,41 @@ public class JobManagerFactory {
         return jobManager;
     }
 
+    private static CustomLogger customLogger = new CustomLogger() {
+
+        @Override
+        public boolean isDebugEnabled() {
+            return true;
+        }
+
+        @Override
+        public void d(String text, Object... args) {
+            Timber.d(String.format(text, args));
+        }
+
+        @Override
+        public void e(Throwable t, String text, Object... args) {
+            Timber.e(t, String.format(text, args));
+        }
+
+        @Override
+        public void e(String text, Object... args) {
+            Timber.e(String.format(text, args));
+        }
+
+        @Override
+        public void v(String text, Object... args) {
+            // no-op
+        }
+    };
+
     private static JobManager configureJobManager(Context context) {
         Configuration.Builder builder = new Configuration.Builder(context)
                 .minConsumerCount(1)//always keep at least one consumer alive
                 .maxConsumerCount(3)//up to 3 consumers at a time
                 .loadFactor(3)//3 jobs per consumer
                 .consumerKeepAlive(120)//wait 2 minutes
-                .customLogger(new CustomLogger() {
-
-                    @Override
-                    public boolean isDebugEnabled() {
-                        return true;
-                    }
-
-                    @Override
-                    public void d(String text, Object... args) {
-                        Timber.d(String.format(text, args));
-                    }
-
-                    @Override
-                    public void e(Throwable t, String text, Object... args) {
-                        Timber.e(t, String.format(text, args));
-                    }
-
-                    @Override
-                    public void e(String text, Object... args) {
-                        Timber.e(String.format(text, args));
-                    }
-
-                    @Override
-                    public void v(String text, Object... args) {
-                        // no-op
-                    }
-                });
+                .customLogger(customLogger);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             builder.scheduler(FrameworkJobSchedulerService.createSchedulerFor(context,
                     SchedulerJobService.class), true);
